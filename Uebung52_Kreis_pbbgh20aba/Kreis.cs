c88dccdd9033e852374f93b06447a2141355bba0
mdbasoglu <69213997+mdbasoglu@users.noreply.github.com>
2021-12-14 18:58:24 +0100

    erstesCommit

---
+++ Uebung52_Kreis_pbbgh20aba/Kreis.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Uebung52_Kreis_pbbgh20aba
+{
+    class Kreis
+    {
+        #region
+        //private double radius;
+
+        //public Kreis(double radius)
+        //{
+        //    this.radius = radius;
+        //}
+
+        //public double Durchmesser()
+        //{
+        //    double d = 2 * radius;
+        //    return d;
+        //}
+
+
+        //public override string ToString()
+        //{
+        //    return $"Kreis[radius={radius},durchmesser={Durchmesser} fläche={..}]";
+        //}
+        #endregion
+
+
+        public void BerechnungderFlaeche(double radius)
+        {
+            double ergebnis = (Math.PI * (radius));
+            Console.WriteLine($"Berechnung der Fläche={ergebnis}");
+        }
+
+        public void BerechnungdesUmfangs(double radius)
+        {
+            double ergebnis = Math.PI * 2 * radius;
+            Console.WriteLine($"Berechnung des Umfangs={ergebnis}");
+        }
+
+        public void BerechnungdesDurchmessers(double radius)
+        {
+            double ergebnis = 2 * radius;
+            Console.WriteLine($"Berechnung des Durchmessers={ergebnis}");
+        }
+
+
+    }
+}
---
+++ Uebung52_Kreis_pbbgh20aba/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uebung52_Kreis_pbbgh20aba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            // Kreis kreisxyz = new Kreis(2.5);
+            // Console.WriteLine(kreisxyz);
+            // double durchmesser = kreisxyz.Durchmesser();
+            // Console.WriteLine(double.durchmesser());
+            #endregion
+
+
+            Kreis Kreis = new Kreis();         // Instanz der Klasse Kreis
+
+            Console.WriteLine("Kreisberechnung\n");
+            Console.Write("Geben Sie den Radius in mm ein!: ");
+            double radius = Convert.ToDouble(Console.ReadLine());  // Eingabe Radius und Konvertierung in double
+
+
+            Kreis.BerechnungderFlaeche(radius);         // Aufruf der Methode Flaeche und Übergabe des Parameters "radius"
+            Kreis.BerechnungdesUmfangs(radius);
+            Kreis.BerechnungdesDurchmessers(radius);
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
